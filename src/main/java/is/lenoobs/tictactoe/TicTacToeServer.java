d32f9d28d809b149ae15f10e2a14c948fe4d680c
Thor Adam <me@thoradam.com>
2013-11-05 18:01:23 +0000

    Source directory structure and TicTacToeServer class

---
+++ src/main/java/is/lenoobs/tictactoe/TicTacToeServer.java
@@ -0,0 +1,11 @@
+package is.lenoobs.tictactoe;
+
+/**
+ * thoradam
+ * 05/11/2013
+ */
+public class TicTacToeServer {
+    public static void main(String[] args) {
+        System.out.println("Tic-tac-toe!");
+    }
+}
