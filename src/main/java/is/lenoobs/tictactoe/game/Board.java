81001332db86f9a5661ea6b62a05d5b51781a745
Gunni <gunnarthors@gmail.com>
2013-11-13 19:14:44 +0000

    added win class to board... checks if there is a winning on the board... unit tests success

--- src/main/java/is/lenoobs/tictactoe/game/Board.java
+++ src/main/java/is/lenoobs/tictactoe/game/Board.java
@@ -34,4 +34,25 @@ public class Board {
 //		int randCell = rand.nextInt(emptyCells.size());
 //		emptyCells.get(randCell)
 //	}
+
+    public String win() {
+        int count = 0;
+        for(int i = 0; i < 9; i++) {
+            if(board[i] == '_')
+                count++;
+        }
+        if(count > 4) return null;
+    
+        int[][] win_rows = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+        
+        for(int i = 0; i < 8; i++) {
+        	if(board[win_rows[i][0]] == 'X' && board[win_rows[i][1]] == 'X' && board[win_rows[i][2]] == 'X')
+        		return "Player Wins!";
+        	else if(board[win_rows[i][0]] == 'O' && board[win_rows[i][1]] == 'O' && board[win_rows[i][2]] == 'O')
+        		return "Computer Wins!" ;
+        	else
+        		return null;
+        }
+        return null;
+    }
 }
